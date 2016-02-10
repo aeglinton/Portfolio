7a08e1357698b3a20a05cdf2c434b994424aaadd
aeglinton <andreaeglinton@Andreas-MacBook-Pro-2.local>
2016-02-09 22:24:25 -0600

    Lots of stuff
    Added actual text, grid of images, new baseman, contact form.

--- about.js
+++ about.js
@@ -3,7 +3,7 @@ var map = L.map('map-container', {
     scrollWheelZoom: false
 });
 map.setView([35, -60], 3);
-L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
 	minZoom: 0,
